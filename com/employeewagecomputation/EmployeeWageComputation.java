fe2eaf0286dc81fe0c764d9e4f79bd0dbc4130f6
rehan7594 <rehansdm94@gmail.com>
2021-04-14 11:51:39 -0700

    Added:welcome message

---
+++ com/employeewagecomputation/EmployeeWageComputation.java
@@ -0,0 +1,8 @@
+package com.employeewagecomputation;
+
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to employee wage computation");
+	}
+}
