b654e9b442c1487171af6b76b511cc300fbf664d
rehan7594 <rehansdm94@gmail.com>
2021-04-14 15:35:58 -0700

    Added: Calculating dailyWage employee

--- com/employeewagecomputation/EmployeeWageComputation.java
+++ com/employeewagecomputation/EmployeeWageComputation.java
@@ -2,23 +2,29 @@ package com.employeewagecomputation;
  
 public class EmployeeWageComputation {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 	
-	
-	//constant variable
+	//Constant 
 	int IS_FULL_TIME = 1;
+	int EMP_RATE_PER_HOUR = 20;
+	
+	//Variables
+	int empHrs = 0;
+	int empWage = 0;
 	
 	//to generate value 0 or 1
 	double empCheck = Math.floor(Math.random()*10) % 2;
 	
 	//checking if employee is present or absent
 	if(empCheck == IS_FULL_TIME)
-	{
-		System.out.println("employee is present");
-	}
+		empHrs = 8;
 	else
-		System.out.println("employee is absent");
+		empHrs = 0;
 	
+	empWage = empHrs * EMP_RATE_PER_HOUR;
+	System.out.println( "Emp Wage: "+ empWage);
+
 	}	
 }
 
