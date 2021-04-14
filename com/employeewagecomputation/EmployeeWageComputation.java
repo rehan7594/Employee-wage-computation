da6a22ccdeda267a5266d2b8d966fac372bed7da
rehan7594 <rehansdm94@gmail.com>
2021-04-14 16:06:11 -0700

    Added: Adding part time employee and Wage

--- com/employeewagecomputation/EmployeeWageComputation.java
+++ com/employeewagecomputation/EmployeeWageComputation.java
@@ -1,29 +1,29 @@
 package com.employeewagecomputation;
- 
+
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) 
 	{
-	
-	//Constant 
-	int IS_FULL_TIME = 1;
-	int EMP_RATE_PER_HOUR = 20;
-	
-	//Variables
-	int empHrs = 0;
-	int empWage = 0;
-	
-	//to generate value 0 or 1
-	double empCheck = Math.floor(Math.random()*10) % 2;
-	
-	//checking if employee is present or absent
-	if(empCheck == IS_FULL_TIME)
-		empHrs = 8;
-	else
-		empHrs = 0;
-	
-	empWage = empHrs * EMP_RATE_PER_HOUR;
-	System.out.println( "Emp Wage: "+ empWage);
+
+		//Constant 
+		int IS_PART_TIME = 1;
+		int IS_FULL_TIME = 2;
+		int EMP_RATE_PER_HOUR = 20;
+
+		//Variables
+		int empHrs = 0;
+		int empWage = 0;
+
+		//to generate value 0 or 1 or 2
+		double empCheck = Math.floor(Math.random()*10) % 3;
+
+		if(empCheck == IS_FULL_TIME)
+			empHrs = 8;
+		else if (empCheck == IS_PART_TIME)
+			empHrs = 4;
+
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println( "Emp Wage: "+ empWage);
 
 	}	
 }
