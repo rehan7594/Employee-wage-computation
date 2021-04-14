7a77b52901c575f22231fba579c2dda9fde9f5b4
rehan7594 <rehansdm94@gmail.com>
2021-04-14 12:14:59 -0700

    Added:checking for employees attendace

--- com/employeewagecomputation/EmployeeWageComputation.java
+++ com/employeewagecomputation/EmployeeWageComputation.java
@@ -2,8 +2,23 @@ package com.employeewagecomputation;
  
 public class EmployeeWageComputation {
 
-	public static void main(String[] args)
+	public static void main(String[] args) {
+	
+	
+	//constant variable
+	int IS_FULL_TIME = 1;
+	
+	//to generate value 0 or 1
+	double empCheck = Math.floor(Math.random()*10) % 2;
+	
+	//checking if employee is present or absent
+	if(empCheck == IS_FULL_TIME)
 	{
-		System.out.println("welcome to employee wage computation");
+		System.out.println("employee is present");
 	}
+	else
+		System.out.println("employee is absent");
+	
+	}	
 }
+
