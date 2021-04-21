d84196416e04e3ad75f500aff392c734226fee43
rehan7594 <rehansdm94@gmail.com>
2021-04-21 04:39:28 -0700

    Added: Wages calculated till 100 hrs or 20 working days reached

--- com/employeewagecomputation/EmployeeWageComputation.java
+++ com/employeewagecomputation/EmployeeWageComputation.java
@@ -9,14 +9,12 @@ total wage for each company
 
 public class EmployeeWageComputation {
 
-	public static final int IS_PART_TIME = 1;
-	public static final int IS_FULL_TIME = 2;	
 
-	private final String company;
-	private final int empRatePerHour;
-	private final int numOfWorkinDays;
-	private final int maxHoursPerMonth;
-	private int totalEmpWage;
+	public final String company;
+	public final int empRatePerHour;
+	public final int numOfWorkinDays;
+	public final int maxHoursPerMonth;
+	public int totalEmpWage;
 
 	public EmployeeWageComputation(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
 	{
@@ -26,48 +24,14 @@ public class EmployeeWageComputation {
 		this.numOfWorkinDays = numOfWorkingDays;
 	}
 
-	public void computeEmpWage()
+	public void setTotalEmpWage(int totalEmpWage)
 	{
-		//variables
-		int empHrs = 0 , totalEmpHours =0 , totalWorkingDays = 0;
-
-		//computation
-		while(totalEmpHours < maxHoursPerMonth && totalWorkingDays < numOfWorkinDays )
-		{
-			totalWorkingDays++;
-			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-
-			switch(empCheck)
-			{
-			case IS_PART_TIME:
-				empHrs = 4;
-				break;
-			case IS_FULL_TIME:
-				empHrs = 8;
-				break;
-			default:
-				empHrs = 0;
-			}
-			totalEmpHours += empHrs;
-			System.out.println("Day:: "+totalWorkingDays+ " Emp Hrs:: "+totalEmpHours);
-		}
-		
-		totalEmpWage = totalEmpHours * empRatePerHour;
+		this.totalEmpWage = totalEmpWage;
 	}
-
 	public String toString()
 	{
 		return "Total wage for company "+ company+ " is " + totalEmpWage;
 	}
-	
-	public static void main(String[] args) {
-		EmployeeWageComputation dMart = new EmployeeWageComputation("Dmart" , 20, 2, 10);
-		EmployeeWageComputation reliance = new EmployeeWageComputation("Reliance" , 10, 4, 20);
-		dMart.computeEmpWage();
-		System.out.println(dMart);
-		reliance.computeEmpWage();
-		System.out.println(reliance);
 
 
-	}
 }
